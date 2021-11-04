dcb0007172542c5079bde4c943cf8c7b9c970fd3
Tatjana Fichtner <tatjana.fichtner@mtp.org>
2021-11-04 10:49:23 +0100

    initialize basic app structure

---
+++ pages/result.tsx
@@ -0,0 +1,12 @@
+export const Result = () => {
+  return (
+    <>
+      <h1>Glückwunsch, du bist ein(e) Schnellleser(in)!</h1>
+      <img></img>
+      <p>Lesegeschwindigkeit:</p>
+      <p>Texterinnerung:</p>
+      <p>Effektivgeschwindigkeit:</p>
+      <button>Zurück zum Start</button>
+    </>
+  );
+};
---
+++ pages/test.tsx
@@ -0,0 +1,45 @@
+export const Test = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit dignissim
+        neque vitae vitae sed diam sociis eu, odio. At egestas id et malesuada.
+        Posuere pellentesque pharetra volutpat eget non in. Massa, turpis
+        adipiscing nisi condimentum a dui. Nec maecenas mi massa mauris justo,
+        ut convallis. Condimentum nunc, ornare id euismod enim mattis feugiat
+        tristique faucibus. Viverra curabitur feugiat dignissim ut faucibus. A,
+        est imperdiet laoreet id mi adipiscing placerat. Tristique vitae
+        adipiscing pellentesque amet congue egestas vitae in sit. Arcu ac
+        posuere fringilla sed. Leo sed laoreet commodo lorem facilisis urna.
+        Porta lacinia malesuada velit tincidunt nec duis imperdiet. Eget elit
+        nisi et et dignissim. Velit amet euismod pellentesque purus. Purus
+        tempor consectetur orci condimentum integer amet, imperdiet. Netus
+        ridiculus amet, congue eget pharetra faucibus vel lectus nulla. Ut
+        ultricies facilisi sodales vel enim. Turpis auctor ornare morbi quam a
+        hac congue tristique. Eget diam sed luctus sociis nulla imperdiet
+        sapien. Arcu blandit felis sit tortor lectus egestas. Id diam
+        ullamcorper neque non adipiscing bibendum nisi nisl enim. Sagittis
+        egestas in eu amet, lorem porttitor condimentum lacus, sollicitudin.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit dignissim
+        neque vitae vitae sed diam sociis eu, odio. At egestas id et malesuada.
+        Posuere pellentesque pharetra volutpat eget non in. Massa, turpis
+        adipiscing nisi condimentum a dui. Nec maecenas mi massa mauris justo,
+        ut convallis. Condimentum nunc, ornare id euismod enim mattis feugiat
+        tristique faucibus. Viverra curabitur feugiat dignissim ut faucibus. A,
+        est imperdiet laoreet id mi adipiscing placerat. Tristique vitae
+        adipiscing pellentesque amet congue egestas vitae in sit. Arcu ac
+        posuere fringilla sed. Leo sed laoreet commodo lorem facilisis urna.
+        Porta lacinia malesuada velit tincidunt nec duis imperdiet. Eget elit
+        nisi et et dignissim. Velit amet euismod pellentesque purus. Purus
+        tempor consectetur orci condimentum integer amet, imperdiet. Netus
+        ridiculus amet, congue eget pharetra faucibus vel lectus nulla. Ut
+        ultricies facilisi sodales vel enim. Turpis auctor ornare morbi quam a
+        hac congue tristique. Eget diam sed luctus sociis nulla imperdiet
+        sapien. Arcu blandit felis sit tortor lectus egestas. Id diam
+        ullamcorper neque non adipiscing bibendum nisi nisl enim. Sagittis
+        egestas in eu amet, lorem porttitor condimentum lacus, sollicitudin.{" "}
+      </p>
+      <button></button>
+    </>
+  );
+};
