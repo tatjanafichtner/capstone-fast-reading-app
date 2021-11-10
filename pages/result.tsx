8848d9e9bc339c4ad7594ff8a4ea4915cfe819cf
Tatjana Fichtner <tatjana.fichtner@mtp.org>
2021-11-10 17:33:54 +0100

    added if-statement for different results

--- pages/result.tsx
+++ pages/result.tsx
@@ -27,6 +27,16 @@ const Result = ({ calculatedTime }: ResultProps) => {
     return result;
   };
 
+  /*
+  _____________________________________________________________
+  Different return statements for slow, medium and fast readers
+  _____________________________________________________________
+  */
+
+  const minimumValueSpeedReader = 0 // TO BE DEFINED!
+  const minimumValueMediumReader = 0 // TO BE DEFINED!
+
+  if ( result > minimumValueSpeedReader ) {
   return (
     <>
       <h1>Glückwunsch, du bist ein(e) Schnellleser(in)!</h1>
@@ -51,7 +61,61 @@ const Result = ({ calculatedTime }: ResultProps) => {
       />
     </>
   );
+}
+} else if ( result > minimumValueMediumReader) {
+  return (
+    <>
+      <h1>Dein Lesevermögen ist durchschnittlich. Ein ordentliches Ergebnis!</h1>
+      <StyledImage
+        alt="medium reader picture"
+        src="/public/Schnellleser.jpg" // Noch austauschen!
+        width={200}
+        height={200}
+      />
+      <p>
+        Lesegeschwindigkeit: {minutes ? 0} {minutes === 1 ? "Minute" : "Minuten"}{" "}
+        und {seconds ? 0} {seconds === 1 ? "Sekunde" : "Sekunden"}
+      </p>
+      <p>
+        Texterinnerung: {score ? 0} von {TOTAL_QUESTIONS}{" "}
+      </p>
+      <p>Effektivgeschwindigkeit: {readingPerformanceResult ? 0}</p>
+      <LinkedButton
+        buttonUrl=""
+        content="Zurück zum Start"
+        id="ButtonResultPage"
+      />
+    </>
+  );
 };
+  
+} else {
+  return (
+    <>
+      <h1>Na, da geht noch was! Weiter so, und du schaffst die Durchschnittsgeschwindigkeit. </h1>
+      <StyledImage
+        alt="slow reader picture"
+        src="/public/Schnellleser.jpg" // Noch austauschen!
+        width={200}
+        height={200}
+      />
+      <p>
+        Lesegeschwindigkeit: {minutes ? 0} {minutes === 1 ? "Minute" : "Minuten"}{" "}
+        und {seconds ? 0} {seconds === 1 ? "Sekunde" : "Sekunden"}
+      </p>
+      <p>
+        Texterinnerung: {score ? 0} von {TOTAL_QUESTIONS}{" "}
+      </p>
+      <p>Effektivgeschwindigkeit: {readingPerformanceResult ? 0}</p>
+      <LinkedButton
+        buttonUrl=""
+        content="Zurück zum Start"
+        id="ButtonResultPage"
+      />
+    </>
+  );
+  
+} 
 
 export default Result;
 
