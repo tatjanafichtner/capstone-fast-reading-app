63180c1f0fc6e8c1b08882b19cb779e11c7c6521
Tatjana Fichtner <tatjana.fichtner@mtp.org>
2021-11-18 10:57:35 +0100

    change webpack config to display SVGs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-};
-
-module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
